95b621fcfd8e2a7b50256197d87319d0224e5d39
Iamthemax <vugore@gmail.com>
2024-02-21 18:35:37 +0530

    after cors indexjs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbObj = require("./db");
 var cors = require('cors')
 require('dotenv').config();
 
-
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
