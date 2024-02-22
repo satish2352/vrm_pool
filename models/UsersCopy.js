563b9872566382c0e4153b34b298c2e0c96cfeea
Iamthemax <vugore@gmail.com>
2024-02-22 19:12:49 +0530

    added_by in models

--- models/UsersCopy.js
+++ models/UsersCopy.js
@@ -33,8 +33,11 @@ const UsersCopy = dbObj.define('users_copy', {
   fileId: {
     type: DataTypes.STRING,
     defaultValue: null,
+  },
+  added_by: {
+    type: DataTypes.STRING,
+    defaultValue: null,
   }
-
   
 });
 
