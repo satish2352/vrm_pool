3ff0f92671beeae9f98fdbf5307c3f1fb14558eb
Iamthemax <vugore@gmail.com>
2024-02-26 18:40:23 +0530

    After static file id for download in downloadcontroller

--- controllers/fileDownloadController.js
+++ controllers/fileDownloadController.js
@@ -6,61 +6,59 @@ const { Op } = require('sequelize');
 const apiResponse = require("../helpers/apiResponse");
 const UsersCopy = require("../models/UsersCopy");
 const path = require('path');
-const excelJS=require("exceljs")
-const workbook = new excelJS.Workbook(); 
+const excelJS = require("exceljs")
+const workbook = new excelJS.Workbook();
 
 const downloadFile = [
   async (req, res) => {
     try {
-     
-      
-      const { fileId} = req.query;
+
+
+      const { fileId } = req.query;
       console.log(fileId)
       const worksheet = workbook.addWorksheet();
       // Fetch reports from the database
       var reports;
       var selectedColumns;
-      if(fileId){
-        selectedColumns = ['fname', 'mname', 'lname', 'email', 'mobile','is_inserted', 'reason','updatedAt'];
+      if (fileId) {
+        selectedColumns = ['fname', 'mname', 'lname', 'email', 'mobile', 'is_inserted', 'reason', 'updatedAt'];
         reports = await UsersCopy.findAll({
-          where:{
-            fileId:fileId
+          where: {
+            fileId: fileId
           }
-        });
-
-        console.log('---------------------------------------------')
-        console.log(reports)
+        });      
         const columns = selectedColumns.map(columnName => ({
           header: columnName.replace(/\s+/g, ''), // Remove spaces from column name
-          key: columnName          
-      }));
+          key: columnName
+        }));
         worksheet.columns = columns;
-      }
-      else{
-        return res.status(400).send({result:false,message:"Enter valid report type"})
-      }
-      
-
-      // If no reports found, send a response with an appropriate message
-      if (!reports.length) {
+        if (!reports.length) {
           console.log("No reports found");
           return apiResponse.successResponse(res, "No reports found", []);
-      }
-       
-      reports.forEach(report => {
+        }
+
+        reports.forEach(report => {
           const rowData = selectedColumns.map(columnName => report[columnName]);
           worksheet.addRow(rowData);
-      });
+        });
 
-      // Write the workbook to the response object 
-      res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); 
-      res.setHeader("Content-Disposition", `attachment; filename=${fileId}.xlsx`);
-      await workbook.xlsx.write(res);
-      res.end();
-  } catch (error) {
+        // Write the workbook to the response object 
+        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        res.setHeader("Content-Disposition", `attachment; filename=${fileId}.xlsx`);
+        await workbook.xlsx.write(res);
+        res.end();
+      }
+      else {
+        return res.status(400).send({ result: false, message: "Enter valid report type" })
+      }
+
+
+      // If no reports found, send a response with an appropriate message
+
+    } catch (error) {
       console.error("Error downloading file:", error);
       return apiResponse.ErrorResponse(res, "Error downloading file");
-  }
+    }
   },
 ];
 
@@ -71,5 +69,5 @@ const downloadFile = [
 
 
 module.exports = {
-    downloadFile,
+  downloadFile,
 };
