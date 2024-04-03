0473a1444bc4ab307ce7b1b4abea1b3ece4d6c51
Iamthemax <vugore@gmail.com>
2024-04-03 16:48:23 +0530

    After Changes in login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -40,7 +40,7 @@ const loginUser = [
             try {
                 const { mobile, password } = req.body;
                 let user = await User.findOne({
-                    where: { mobile: mobile,is_active:1},
+                    where: { mobile: mobile,is_active:1,is_delete:0,user_type:"1 || 2"},
                 });
                 if(!user){
 
