4ecd9f20e9e0412272ac778ba22dd2faabbf1c6a
Iamthemax <vugore@gmail.com>
2024-04-04 11:50:03 +0530

    After user match but not db entry

--- controllers/agentDataDownloadController.js
+++ controllers/agentDataDownloadController.js
@@ -1,6 +1,6 @@
 const verifyToken = require("../middleware/verifyToken");
 const AgentData = require("../models/AgentData");
-const User = require("../models/Users");
+const Users = require("../models/Users");
 const { validationResult } = require("express-validator");
 const { Op, fn, col ,literal} = require('sequelize'); // Importing Op, fn, and col from sequelize
 const apiResponse = require("../helpers/apiResponse");
@@ -49,9 +49,61 @@ const downloadFile = (url, destination) => {
     return new Promise((resolve, reject) => {
       try {
         const results = [];
+        const matchedResults = [];
         fs.createReadStream(filePath)
           .pipe(csv())
-          .on('data', (data) =>{results.push(data)}) 
+          .on('data', async (data) => {
+
+            if(data.AgentPhoneNumber){
+              await Users.findOne({
+                where: { mobile: data.AgentPhoneNumber.slice(-10) },
+              })
+              .then(user => {
+                console.log('user:', user);
+                if (user) {
+                  data.user_id = user.id.toString();
+                  matchedResults.push(data)
+                }
+              })
+              .catch(error => {
+                console.error('Error finding user:', error);
+                res.status(500).send({ result: false, message: 'Error finding user: ', error });
+              });
+              console.log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+              console.log(matchedResults)
+              resolve(matchedResults);
+
+            }          
+          })
+          .on('end', () => resolve(results))
+          .on('error', (error) => reject(error));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+
+  const readCSVFileWithUserIds = (filePath) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const results = [];
+        const users = await Users.findAll; // Fetch user details from the database
+        const userMap = new Map(users.map(user => [user.mobile, user.id])); // Create a map of mobile numbers to user IDs
+  
+        fs.createReadStream(filePath)
+          .pipe(csv())
+          .on('data', (data) => {
+            // Check if the AgentPhoneNumber exists in the userMap
+            if (userMap.has(data.AgentPhoneNumber)) {
+              // If yes, add the user_id to the data
+              data.user_id = userMap.get(data.AgentPhoneNumber);
+            }
+            results.push(data);
+
+            console.log("11111111111111111111111111111111111111111111111111111111")
+            console.log(results)
+
+          })
           .on('end', () => resolve(results))
           .on('error', (error) => reject(error));
       } catch (error) {
@@ -79,6 +131,8 @@ const downloadFile = (url, destination) => {
   };
 
   const insertDataToAgentData = async (data) => {
+
+    console.log("*******@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*****")
     try {
       await AgentData.bulkCreate(data);
       console.log('Data inserted successfully into AgentData table');
