b4a38a190267e34f67d9b0f1fd7d57d40a4b8bbf
Iamthemax <vugore@gmail.com>
2024-04-16 18:12:01 +0530

    afte getAllReports Changes

--- controllers/getAllReportsController.js
+++ controllers/getAllReportsController.js
@@ -5,6 +5,7 @@ const { body, query, validationResult } = require("express-validator");
 const sequelize = require('../db');
 const { Op } = require('sequelize');
 const apiResponse = require("../helpers/apiResponse");
+const moment = require('moment-timezone');
 
 
 const getAllReports = [
@@ -14,7 +15,7 @@ const getAllReports = [
     const user_type = req.user_type; // Filter by role
 
     try {
-      const { user_type, fromdate, todate, status, id } = req.body;
+      const { user_type, fromdate, todate, status, id,supervisor_id,agent_id,direction ,fromtime,totime,time} = req.body;
 
       // Step 1: Filter users based on user_type if provided
       let userFilter = {
@@ -45,14 +46,40 @@ const getAllReports = [
       };
 
       // Add optional filters if provided by the user
-      if (fromdate && todate) {
-        reportFilter.start_time = {
-          [Op.between]: [fromdate, todate] // Filter by createdAt between fromdate and todate
-        };
-      }
-      if (status) {
-        reportFilter.status = status; // Filter by status
-      }
+      if ((fromdate && todate) &&  (! fromtime && ! totime)) {
+                reportFilter.updatedAt = {
+                    [Op.between]: [fromdate+" 00:00:00", todate+" 23:59:59"]
+                };
+            }
+
+            if ((fromdate && todate) && (fromtime && totime)) {
+                reportFilter.updatedAt = {
+                    [Op.between]: [fromdate+" "+fromtime+":00", todate+" "+totime+":59"]
+                };
+            }
+            if (status) {
+                reportFilter.status = status;
+            }
+
+            if (direction) {
+                reportFilter.direction = direction;
+            }
+            if (time) {
+                // Calculate the current date and time in the 'Asia/Kolkata' timezone
+                const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+        
+                // Calculate the date and time 'minutes' minutes ago in the 'Asia/Kolkata' timezone
+                const minutesAgoDateTime = moment().subtract(time, 'minutes').format('YYYY-MM-DD HH:mm:ss');
+        
+                // Construct the query
+                reportFilter.updatedAt = {
+                    [Op.between]: [minutesAgoDateTime, currentDateTime]
+                };
+        
+                // Example: execute your query using Sequelize or perform any other actions needed
+            }
+
+
 
       const reports = await AgentData.findAll({
         where: reportFilter,
