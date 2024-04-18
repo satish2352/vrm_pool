9930ccec465bafd960af7ce59fdb48facd71a2a2
Iamthemax <vugore@gmail.com>
2024-04-18 11:26:51 +0530

    After try catch

--- controllers/uploadSuperviserController.js
+++ controllers/uploadSuperviserController.js
@@ -52,6 +52,7 @@ const uploadSupervisers = [
       return res.status(400).json({ result: false, message: 'To upload file select valid file. only .xlsx or .xls file is allowed.' });
     }
     try {
+      console.log("---------------------------------")
     console.log(req.file.path)
     const workbook = xlsx.readFile(req.file.path);
     const sheets = workbook.SheetNames;
