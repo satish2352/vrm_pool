81b700a7005debce9ec41b01b0493092e3481b50
Iamthemax <vugore@gmail.com>
2024-04-18 16:01:32 +0530

    After changes for db.js new code

--- db.js
+++ db.js
@@ -1,26 +1,3 @@
-const { Sequelize } = require("sequelize");
-require('dotenv').config();
-
-const ENVIRONMENT = process.env.ENVIRONMENT;
-const DB_NAME=process.env.DB_NAME
-const DB_USER=process.env.DB_USER
-const DB_PASSWORD=process.env.DB_PASSWORD
-const DB_HOST=process.env.DB_HOST
-const DB_PORT=process.env.DB_PORT
-
-let dbObj;
-
-  dbObj = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    dialect: 'mysql',
-    host: DB_HOST,
-    port:DB_PORT
-  });
-
-
-module.exports = dbObj
-
-
-// Satish code 
 // const { Sequelize } = require("sequelize");
 // require('dotenv').config();
 
@@ -33,19 +10,42 @@ module.exports = dbObj
 
 // let dbObj;
 
-// // Initialize Sequelize instance
-// dbObj = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-//   dialect: 'mysql',
-//   host: DB_HOST,
-//   port: DB_PORT
-// });
-
-// // Export dbObj after it has been properly initialized
-// dbObj.authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//     module.exports = dbObj;
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
+//   dbObj = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//     dialect: 'mysql',
+//     host: DB_HOST,
+//     port:DB_PORT
 //   });
+
+
+// module.exports = dbObj
+
+
+// Satish code 
+const { Sequelize } = require("sequelize");
+require('dotenv').config();
+
+const ENVIRONMENT = process.env.ENVIRONMENT;
+const DB_NAME=process.env.DB_NAME
+const DB_USER=process.env.DB_USER
+const DB_PASSWORD=process.env.DB_PASSWORD
+const DB_HOST=process.env.DB_HOST
+const DB_PORT=process.env.DB_PORT
+
+let dbObj;
+
+// Initialize Sequelize instance
+dbObj = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  dialect: 'mysql',
+  host: DB_HOST,
+  port: DB_PORT
+});
+
+// Export dbObj after it has been properly initialized
+dbObj.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    module.exports = dbObj;
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
