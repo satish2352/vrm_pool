9392e0837ebbb22d9fc4bbd31d90b28c3cfe0e9c
Iamthemax <vugore@gmail.com>
2024-04-25 19:33:06 +0530

    after changes in time from singlerow controller

--- controllers/agentReportsSingleRowController.js
+++ controllers/agentReportsSingleRowController.js
@@ -71,9 +71,10 @@ const getAgentReportsSingleRow = [
                 fromTimeNew=new Date(fromTimeNew.getTime() - 60 * 60000)
                 toTimeNew=new Date(toTimeNew.getTime() - 60 * 60000)
                 const fromTimeUTC = new Date(fromTimeNew.getTime() - fromTimeNew.getTimezoneOffset() * 60000);
+                const toTimeUTC = new Date(toTimeNew.getTime() - toTimeNew.getTimezoneOffset() * 60000);
 
-// Convert toTimeNew to UTC
-const toTimeUTC = new Date(toTimeNew.getTime() - toTimeNew.getTimezoneOffset() * 60000);
+                console.log(fromTimeUTC)
+                console.log(toTimeUTC)
                 reportFilter.updatedAt = {
                     //[Op.between]: [fromdate+" "+fromtime+":00", todate+" "+totime+":59"]
                     [Op.between]: [fromTimeUTC, toTimeUTC]
