93019cd4847cefa69d36874325766f7083b4c15e
Iamthemax <vugore@gmail.com>
2024-04-30 17:13:08 +0530

    changes for sh file in project added

---
+++ scheduler.js
@@ -0,0 +1,18 @@
+const cron = require('node-cron');
+const { exec } = require('child_process');
+
+// Schedule the job to run on the first day of every month at 11:59 PM
+const job = cron.schedule('* * * * *', () => {
+  // Execute your shell script
+  exec('sh dumpdata.sh', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the script: ${error}`);
+    }
+    console.log(`Script output: ${stdout}`);
+    console.error(`Script errors: ${stderr}`);
+  });
+}, {
+  timezone: 'Asia/Kolkata' // Specify your timezone here
+});
+
+job.start(); // Start the cron job
