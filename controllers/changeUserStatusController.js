031665cf84f87cce2f4e92b0269a703344a9e10d
Iamthemax <vugore@gmail.com>
2024-05-17 12:06:40 +0530

    user not active when supervisor is deleted updated new

--- controllers/changeUserStatusController.js
+++ controllers/changeUserStatusController.js
@@ -36,15 +36,16 @@ const changeUserStatus = [
                             return res.status(404).json({ result: false, message: "User not found" });
                         }
                         if (user) {
+                            var conditionCount;
                             if(user.user_type ===3) {
-                                const conditionCount = await User.count({
+                                conditionCount = await User.count({
                                     where: {
                                         added_by: user.added_by,                                
                                         is_active:0,                                                       
                                     }
                                 });
                             } else {
-                                const conditionCount = await User.count({
+                                conditionCount = await User.count({
                                     where: {
                                         added_by: user.id,                                
                                         is_active:0,                                                       
