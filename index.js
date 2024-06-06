d2aa078162176355798707932e4ff87e5a27f067
Iamthemax <vugore@gmail.com>
2024-06-06 15:49:28 +0530

    After change port

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ const dbObj = require("./db");
 // CORS middleware
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
