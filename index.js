ada447366bc70d1a997b20a7024cf4dcb1c0a9b7
Iamthemax <vugore@gmail.com>
2024-06-07 15:54:49 +0530

    After change in index js

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
 // API routes
-app.all("/api/", apiRouter);
+app.use("/api/", apiRouter);
 
 module.exports = app;
